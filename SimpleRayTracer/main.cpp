11048add6bcd0012f585a5f51b0c3ea28e3bf053
andreskballero <acaballerotoledo@gmail.com>
2020-06-12 21:11:53 +0200

    Initial Commit

---
+++ SimpleRayTracer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SimpleRayTracer
+//
+//  Created by Andrés Caballero Toledo on 12/06/2020.
+//  Copyright © 2020 act. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
